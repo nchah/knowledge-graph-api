d7eaf4369d2a26f00f3b2ee0230fff0fe56483c9
Niel C <nielchah@gmail.com>
2016-04-21 20:18:58 -0400

    Adding script

---
+++ kgi-api.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Knowledge Graph API
+https://developers.google.com/knowledge-graph/
+
+"""
+
+import datetime
+import requests
+import json
+import urllib
+
+# Creating the timestamp for the output filename
+
+# The API Key and Service URL
+api_key = open("", "r")
+service_url = ""
+
+
+"""Example of Python client calling Knowledge Graph Search API."""
+
+api_key = open('.api_key').read()
+query = 'Taylor Swift'
+service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+params = {
+    'query': query,
+    'limit': 10,
+    'indent': True,
+    'key': api_key,
+}
+url = service_url + '?' + urllib.urlencode(params)
+response = json.loads(urllib.urlopen(url).read())
+for element in response['itemListElement']:
+    print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
+
+"""
+Sample result: https://kgsearch.googleapis.com/v1/entities:search?query=taylor+swift&key=[]&limit=1&indent=True
+
+{
+  "@context": {
+    "@vocab": "http://schema.org/",
+    "goog": "http://schema.googleapis.com/",
+    "EntitySearchResult": "goog:EntitySearchResult",
+    "detailedDescription": "goog:detailedDescription",
+    "resultScore": "goog:resultScore",
+    "kg": "http://g.co/kg"
+  },
+  "@type": "ItemList",
+  "itemListElement": [
+    {
+      "@type": "EntitySearchResult",
+      "result": {
+        "@id": "kg:/m/0dl567",
+        "name": "Taylor Swift",
+        "@type": [
+          "Thing",
+          "Person"
+        ],
+        "description": "Singer-songwriter",
+        "image": {
+          "contentUrl": "http://t1.gstatic.com/images?q=tbn:ANd9GcQmVDAhjhWnN2OWys2ZMO3PGAhupp5tN2LwF_BJmiHgi19hf8Ku",
+          "url": "https://en.wikipedia.org/wiki/Taylor_Swift",
+          "license": "http://creativecommons.org/licenses/by-sa/2.0"
+        },
+        "detailedDescription": {
+          "articleBody": "Taylor Alison Swift is an American singer-songwriter.
+          Raised in Wyomissing, Pennsylvania, she moved to Nashville, Tennessee, at the age of 14
+          to pursue a career in country music. ",
+          "url": "http://en.wikipedia.org/wiki/Taylor_Swift",
+          "license": "https://en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License"
+        },
+        "url": "http://www.taylorswift.com/"
+      },
+      "resultScore": 884.364868
+    }
+  ]
+}
+"""
+
+
