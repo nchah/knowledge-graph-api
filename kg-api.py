18aad07e471a085fdc82f675851cf40d2e57e336
Niel C <nielchah@gmail.com>
2016-06-29 22:47:38 -0400

    Adding KG API code done during Web Archives 2.0 Datathon

--- kg-api.py
+++ kg-api.py
@@ -1,34 +1,78 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """
-Knowledge Graph API https://developers.google.com/knowledge-graph/
+Query the Knowledge Graph API https://developers.google.com/knowledge-graph/
 
 """
 
+import argparse
 import datetime
 import requests
 import json
 import urllib
 
-# Creating the timestamp for the output filename
+
+def main(query):
+    api_key = open('.api_key').read()
+    service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+
+    params = {
+        'query': query,
+        'limit': 5,
+        'indent': True,
+        'key': api_key,
+        }
+
+    url = service_url + '?' + urllib.urlencode(params)  # TODO: use requests
+    response = json.loads(urllib.urlopen(url).read())
+
+    # Parsing the response  TODO: log all responses
+    print('Displaying results...' + ' (limit: ' + str(params['limit']) + ')\n')
+    for element in response['itemListElement']:
+        try:
+            types = str(", ".join([n for n in element['result']['@type']]))
+        except KeyError:
+            types = "N/A"
+
+        try:
+            desc = str(element['result']['description'])
+        except KeyError:
+            desc = "N/A"
+
+        try:
+            detail_desc = str(element['result']['detailedDescription']['articleBody'])[0:100] + '...'
+        except KeyError:
+            detail_desc = "N/A"
+
+        try:
+            mid = str(element['result']['@id'])
+        except KeyError:
+            mid = "N/A"
+
+        try:
+            url = str(element['result']['url'])
+        except KeyError:
+            url = "N/A"
+
+        try:
+            score = str(element['resultScore'])
+        except KeyError:
+            score = "N/A"
+
+        print(element['result']['name'] \
+                + '\n' + ' - entity_types: ' + types \
+                + '\n' + ' - description: ' + desc \
+                + '\n' + ' - detailed_description: ' + detail_desc \
+                + '\n' + ' - identifier: ' + mid \
+                + '\n' + ' - url: ' + url \
+                + '\n' + ' - resultScore: ' + score \
+                + '\n')
 
 
-# The API Key and Service URL
-api_key = open('.api_key').read()
-service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
-
-query = ''
-params = {
-    'query': query,
-    'limit': 10,
-    'indent': True,
-    'key': api_key,
-    }
-
-url = service_url + '?' + urllib.urlencode(params)  # TODO: use requests
-response = json.loads(urllib.urlopen(url).read())
-for element in response['itemListElement']:
-    print(element['result']['name'] + ' (' + str(element['resultScore']) + ')')
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('query', help='The search term to query')
+    args = parser.parse_args()
+    main(args.query)
 
 
 """
@@ -74,5 +118,4 @@ Sample result: https://kgsearch.googleapis.com/v1/entities:search?query=taylor+s
   ]
 }
 """
-
 
